65e390e2a42a9cd1d3fc2af09c305208ff29e5ed
ghagevaibhav <ghagevaibhav@gmail.com>
2023-12-15 15:33:12 +0530

    last phase

---
+++ index.js
@@ -0,0 +1,46 @@
+var timer = 60;
+var score = 0;
+var hit = 0;
+
+function score(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function newHit(){
+    hit = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hit;
+}
+
+function bubbles()
+{   var clutter = "";
+    for(var i = 0; i <= 183; i++){
+        clutter+=`<div id="bubble">${Math.floor(Math.random() * 10)}</div>`
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer(){
+    var timerStart = setInterval(()=>{
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else{
+            clearInterval(timerStart);
+        }
+    }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", (dets)=>{
+    var num = Number(dets.target.textContent)
+    if(hit == num) score();
+    bubbles();
+    newHit();
+})
+
+
+newHit()
+runTimer();
+bubbles();
